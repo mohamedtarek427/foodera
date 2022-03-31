dc0aaf6d316a264259fb5dd9277d0931a1ef58e5
mohamedtarek427 <102771793+mohamedtarek427@users.noreply.github.com>
2022-03-31 16:56:42 -0700

    Add files via upload

---
+++ component/Home/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="col-md-6">
+                    <h2>good food choices are good investment</h2>
+                    <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+                    <button>order now</button>
+                    <button>learn more</button>
+
+                </div>
+                <div className="col-md-6">
+
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ data.js
@@ -0,0 +1,25 @@
+import blog1 from './asstets/01.jpg'
+import blog2 from './asstets/02.jpg'
+import blog3 from './asstets/03.jpg'
+
+const Data = [
+    {
+        img: blog1 ,
+        title: 'Rainbow Vegetable Sandwich',
+        time:'15 - 20 Minutes',
+        price: '$10.50$',
+    },
+    {
+        img: blog2,
+        title: 'Vegetarian Burger',
+        time:': 30 - 45 Minutes',
+        price: '$9.20$',
+    },
+    {
+        img: blog3,
+        title: 'Raspberry Stuffed French Toast',
+        time:'10 - 15 Minutes',
+        price: '$12.50$',
+    },
+]
+export default Data
